1ef0bf6ab2bee144312dd958b29b9ec65768c5cf
ezhilsabareesh8 <ezhilsabareesh@gmail.com>
2023-10-17 13:41:34 +1100

    Changes in WW3 for running on OM2 grid and JRA55do

--- WW3_PreProc/GenGrid.py
+++ WW3_PreProc/GenGrid.py
@@ -1,7 +1,7 @@
 """
 WWIII Grid Generation Script
 
-This script generates a grid from an Ocean_hgrid file for use with the WAVEWATCH III (WW3) model.
+This script generates a grid from OM2 Ocean_hgrid and topog files for use with the WAVEWATCH III (WW3) model.
 It is adapted from Shou Li's config and modified to work with the current version of WW3 in access-OM3.
 
 Author: Ezhilsabareesh Kannadasan
@@ -9,7 +9,7 @@ Author: Ezhilsabareesh Kannadasan
 Usage:
     python generate_grid.py
 
-Input: ocean_hgrid.nc, ocean_topog.nc
+Input: ocean_hgrid.nc, topog.nc
 Output: dat files containing Lat, Lon, Mask and Obstruction grid
 
 """
@@ -17,13 +17,19 @@ Output: dat files containing Lat, Lon, Mask and Obstruction grid
 import numpy as np
 import netCDF4 as NC
 
-grid_handle = NC.Dataset('/g/data/ik11/inputs/cime/inputdata/2023-03-10/ocn/mom/gx1v6/ocean_hgrid.nc', mode='r')
-topog_handle = NC.Dataset('/g/data/ik11/inputs/cime/inputdata/2023-03-10/ocn/mom/gx1v6/ocean_topog.nc', mode='r')
+grid_handle = NC.Dataset('/g/data/ik11/inputs/access-om3/0.x.0/1deg/mom/ocean_hgrid.nc', mode='r')
+topog_handle = NC.Dataset('/g/data/ik11/inputs/access-om3/0.x.0/1deg/share/topog.nc', mode='r')
 
 # Read in Grid
 Lon = grid_handle.variables['x'][:, :]
 Lat = grid_handle.variables['y'][:, :]
 H_DPT = topog_handle.variables['depth'][:, :]
+
+# Convert the data to a NumPy array
+H_DPT = np.array(H_DPT)
+
+# Replace masked values with zeros
+H_DPT[H_DPT == -9999.0] = 0.0
 
 print(np.shape(H_DPT))
 
@@ -38,7 +44,7 @@ H_LON = Lon[1::2, 1::2]
 LLAT = len(H_LAT[:, 0])
 LLON = len(H_LAT[0, :])
 
-GRIDNAME = 'OM3_1'
+GRIDNAME = 'OM2_1'
 
 with open(GRIDNAME + '.Mask', 'w') as f6, open(GRIDNAME + '.Dpt', 'w') as f7, open(GRIDNAME + '.Obstr', 'w') as f9, open(
         GRIDNAME + '.Lat', 'w') as f10, open(GRIDNAME + '.Lon', 'w') as f11:
