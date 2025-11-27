277b73fe10da0a794685dc9160fd42630f87d9bb
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-11-27 17:28:06 +1100

    Updates for RTD

--- documentation/hooks/hide_pages_url_segment.py
+++ documentation/hooks/hide_pages_url_segment.py
@@ -4,6 +4,8 @@ from mkdocs.plugins import event_priority
 @event_priority(-100)
 def on_page_markdown(markdown, *, page, config, files):
     if page.file.url.startswith("pages/"):
+        # Remove "pages/" prefix from the URL
         page.file.url = page.file.url.removeprefix("pages/")
         page.file.dest_uri = page.file.dest_uri.removeprefix("pages/")
         page.file.abs_dest_path = page.file.abs_dest_path.removeprefix("pages/")
+        page.canonical_url = config.site_url + page.file.url
