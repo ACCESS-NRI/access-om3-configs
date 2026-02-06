51252957e2a921f27f57f7df585ff1f2a24570c5
Christopher Bull <5499680+chrisb13@users.noreply.github.com>
2026-02-06 21:50:22 +1100

    dropped an experiment so picking it up

--- documentation/docs/copy_files_for_push.sh
+++ documentation/docs/copy_files_for_push.sh
@@ -1,5 +1,6 @@
 set -x
 cd /home/561/cyb561/repos/access-om3-configs/documentation/docs/assets/experiments
+mkdir -p MC_25km_jra_iaf-1.0-beta-5165c0f8
 mkdir -p MC_25km_jra_iaf-1.0-beta-gm1-d968c801
 mkdir -p MC_25km_jra_iaf-1.0-beta-gm2-5dc49da6
 mkdir -p MC_25km_jra_iaf-1.0-beta-gm3-da330542
@@ -17,6 +18,7 @@ cp ${src}mkfigs_output_MC_25km_jra_iaf-1.0-beta-gm4-9fd08880/mkmd/*.png  ${dest}
 cp ${src}mkfigs_output_MC_25km_jra_iaf-1.0-beta-gm5-9b5dbfa9/mkmd/*.png  ${dest}MC_25km_jra_iaf-1.0-beta-gm5-9b5dbfa9
 
 cd /home/561/cyb561/repos/access-om3-configs/documentation/docs/pages/experiments
+mkdir -p MC_25km_jra_iaf-1.0-beta-5165c0f8
 mkdir -p MC_25km_jra_iaf-1.0-beta-gm1-d968c801
 mkdir -p MC_25km_jra_iaf-1.0-beta-gm2-5dc49da6
 mkdir -p MC_25km_jra_iaf-1.0-beta-gm3-da330542
